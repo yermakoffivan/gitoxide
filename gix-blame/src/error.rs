8d3c7402025d613e738cc4e4feee3a453901a626
Byron <sebastian.thiel@icloud.com>
2026-08-20 17:12:17 +0200

    change(gix-blame)!: migrate errors to gix-error

--- gix-blame/src/error.rs
+++ gix-blame/src/error.rs
@@ -1,40 +1,93 @@
 use gix_object::bstr::BString;
 
 /// The error returned by [file()](crate::file()).
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug)]
 #[expect(missing_docs)]
 pub enum Error {
-    #[error("No commit was given")]
     EmptyTraversal,
-    #[error(transparent)]
-    BlobDiffSetResource(#[from] gix_diff::blob::platform::set_resource::Error),
-    #[error(transparent)]
-    BlobDiffPrepare(#[from] gix_diff::blob::platform::prepare_diff::Error),
-    #[error("The file to blame at '{file_path}' wasn't found in the first commit at {commit_id}")]
+    BlobDiffSetResource(gix_diff::blob::platform::set_resource::Error),
+    BlobDiffPrepare(gix_diff::blob::platform::prepare_diff::Error),
     FileMissing {
         /// The file-path to the object to blame.
         file_path: BString,
         /// The commit whose tree didn't contain `file_path`.
         commit_id: gix_hash::ObjectId,
     },
-    #[error("Couldn't find commit or tree in the object database")]
-    FindObject(#[from] gix_object::find::Error),
-    #[error("Could not find existing blob or commit")]
-    FindExistingObject(#[from] gix_object::find::existing_object::Error),
-    #[error("Could not find existing iterator over a tree")]
-    FindExistingIter(#[from] gix_object::find::existing_iter::Error),
-    #[error("Failed to obtain the next commit in the commit-graph traversal")]
-    Traverse(#[source] Box<dyn std::error::Error + Send + Sync>),
-    #[error(transparent)]
-    DiffTree(#[from] gix_diff::tree::Error),
-    #[error(transparent)]
-    DiffTreeWithRewrites(#[from] gix_diff::tree_with_rewrites::Error),
-    #[error(
-        "Invalid line range was given, line range is expected to be a 1-based inclusive range in the format '<start>,<end>'"
-    )]
+    FindObject(gix_object::find::Error),
+    FindExistingObject(gix_object::find::existing_object::Error),
+    FindExistingIter(gix_object::find::existing_iter::Error),
+    Traverse(Box<dyn std::error::Error + Send + Sync>),
+    DiffTree(gix_diff::tree::Error),
+    DiffTreeWithRewrites(gix_diff::tree_with_rewrites::Error),
     InvalidOneBasedLineRange,
-    #[error("Failure to decode commit during traversal")]
-    DecodeCommit(#[from] gix_object::decode::Error),
-    #[error("Failed to get parent from commitgraph during traversal")]
-    GetParentFromCommitGraph(#[from] gix_error::Message),
+    DecodeCommit(gix_object::decode::Error),
+    GetParentFromCommitGraph(gix_error::Message),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::EmptyTraversal => f.write_str("No commit was given"),
+            Error::BlobDiffSetResource(err) => std::fmt::Display::fmt(err, f),
+            Error::BlobDiffPrepare(err) => std::fmt::Display::fmt(err, f),
+            Error::FileMissing {
+                file_path,
+                commit_id,
+            } => write!(
+                f,
+                "The file to blame at '{file_path}' wasn't found in the first commit at {commit_id}"
+            ),
+            Error::FindObject(_) => f.write_str("Couldn't find commit or tree in the object database"),
+            Error::FindExistingObject(_) => f.write_str("Could not find existing blob or commit"),
+            Error::FindExistingIter(_) => f.write_str("Could not find existing iterator over a tree"),
+            Error::Traverse(_) => f.write_str("Failed to obtain the next commit in the commit-graph traversal"),
+            Error::DiffTree(err) => std::fmt::Display::fmt(err, f),
+            Error::DiffTreeWithRewrites(err) => std::fmt::Display::fmt(err, f),
+            Error::InvalidOneBasedLineRange => f.write_str(
+                "Invalid line range was given, line range is expected to be a 1-based inclusive range in the format '<start>,<end>'",
+            ),
+            Error::DecodeCommit(_) => f.write_str("Failure to decode commit during traversal"),
+            Error::GetParentFromCommitGraph(_) => {
+                f.write_str("Failed to get parent from commitgraph during traversal")
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::BlobDiffSetResource(err) => err.source(),
+            Error::BlobDiffPrepare(err) => err.source(),
+            Error::FindObject(err) => Some(err.as_ref()),
+            Error::FindExistingObject(err) => Some(err),
+            Error::FindExistingIter(err) => Some(err),
+            Error::Traverse(err) => Some(err.as_ref()),
+            Error::DiffTree(err) => err.source(),
+            Error::DiffTreeWithRewrites(err) => err.source(),
+            Error::DecodeCommit(err) => Some(err),
+            Error::GetParentFromCommitGraph(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+macro_rules! from_error {
+    ($ty:ty, $variant:ident) => {
+        impl From<$ty> for Error {
+            fn from(err: $ty) -> Self {
+                Error::$variant(err)
+            }
+        }
+    };
+}
+
+from_error!(gix_diff::blob::platform::set_resource::Error, BlobDiffSetResource);
+from_error!(gix_diff::blob::platform::prepare_diff::Error, BlobDiffPrepare);
+from_error!(gix_object::find::Error, FindObject);
+from_error!(gix_object::find::existing_object::Error, FindExistingObject);
+from_error!(gix_object::find::existing_iter::Error, FindExistingIter);
+from_error!(gix_diff::tree::Error, DiffTree);
+from_error!(gix_diff::tree_with_rewrites::Error, DiffTreeWithRewrites);
+from_error!(gix_object::decode::Error, DecodeCommit);
+from_error!(gix_error::Message, GetParentFromCommitGraph);
